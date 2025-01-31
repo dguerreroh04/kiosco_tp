32faa8579243f1b1b71a85e0323135dd2b4c235e
sebastian colazo <scolazo@fi.uba.ar>
2025-01-31 20:52:55 -0300

    Conecte ticket.js en app.js e implemente algunas funciones basicas para ticket.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const productos = require('./routes/productos')
 const usuarios = require('./routes/usuarios')
+const ticket = require('./routes/ticket')
 const app = express()
 const port = 3000
 
@@ -13,6 +14,9 @@ app.get('/',(req, res) =>{
 app.use('/api/v1/productos',productos)
 
 app.use('/api/v1/usuarios',usuarios) 
+
+app.use('/api/v1/ticket',ticket) 
+
 
 app.listen(port, () => {
     console.log(`Kiosco app listening on port ${port}`);
