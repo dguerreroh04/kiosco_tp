35b6074540f856056a9b3f1ad3ecc64c9d1cc8a9
sebastian colazo <scolazo@fi.uba.ar>
2025-02-05 00:23:25 -0300

    agregue funcionalidad a prod_selec

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require('@prisma/client')
 const express = require('express')
-const ticket = require('./routes/ticket')
+const productos_seleccionados = require('./routes/productos_seleccionados')
 const app = express()
 const port = 3000
 
@@ -12,7 +12,7 @@ app.get('/',(req, res) =>{
     res.send('Kiosco')
 })
 
-app.use('/api/v1/ticket',ticket) 
+app.use('/api/v1/productos_seleccionados',productos_seleccionados) 
 
 app.listen(port, () => {
     console.log(`Kiosco app listening on port ${port}`);
--- src/routes/productos_seleccionados.js
+++ src/routes/productos_seleccionados.js
@@ -1,1 +1,45 @@
-//prueba de nuevo back
+const { PrismaClient } = require('@prisma/client')
+const express = require('express')
+const router = express.Router()
+const prisma = new PrismaClient()
+
+//para obtener todos los productos selecionados
+router.get('/', async (req, res) => {
+    try{
+        const productos_seleccionados = await prisma.productoSeleccionado.findmany()
+        res.json(productos_seleccionados)
+    }catch(error){
+        res.status(500).json({mensaje:'Error al obtener los productos seleccionados',error})
+    }
+})
+
+
+//actualizar con id_comprador e id_producto
+router.put('/:id_comprador/:id_producto', async (req,res)=> {
+    const {id_comprador,id_producto} = req.params
+    const {cantidad} = req.body
+
+    if(!cantidad || cantidad <= 0){
+        return res.status(400).json({mensaje:'Ingrese una cantidad mayor a cero'})
+    }
+
+    try{
+        const productoSeleccionado = await prisma.productoSeleccionado.findUnique({
+            where:{id_comprador_id_producto:{id_comprador: Number(id_comprador), id_producto: Number(id_producto)}}
+        })
+
+        if(!productoSeleccionado){
+            return res.status(404).json({mensaje:'No se encontro el producto'})
+        }
+
+        const producto_actualizado = await prisma.productoSeleccionado.update({
+            where: {id_comprador_id_producto:{id_comprador: Number(id_comprador), id_producto: Number(id_producto)}},
+            data:{cantidad}
+        })
+        res.json({mensaje:'Se actualizo el producto',producto: producto_actualizado})
+    }catch(error){
+        res.status(500).json({mensaje:'Error al obtener la informacion',error})
+    }
+})
+
+module.exports = router
