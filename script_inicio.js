3cef89ab7423a7ed635dc46f128e1f6822c1c64c
Diego Guerrero <dguerrerog@fi.uba.ar>
2025-02-05 18:45:01 -0300

    Correccion de errores de script de inicio

--- script_inicio.js
+++ script_inicio.js
@@ -9,22 +9,29 @@ function showProducts() {
 
         productos.forEach(producto => {
             let container = document.getElementById('container_' + producto.categoria);
-            let ul = container.querySelector('ul');
-            let productoDiv = document.createElement('div');
-            productoDiv.classList.add('card', 'border-info', 'mb-3');
-            productoDiv.style.width = 'fit-content';
-            productoDiv.style.height = 'min-content';
-            productoDiv.innerHTML = `
-                <img src="${producto.imagen}" alt="${producto.nombre}" style="width: 180px;">
-            <h5 class="card-title">${producto.nombre}</h5>
-                <p class="card-text">${producto.precio}</p>
-                <p class="card-text">${producto.descripcion}</p>
-                <button type="button" class="btn btn-primary" onclick="añadir_carrito(${producto.id})">Añadir al carrito</button>
-            `;
-            ul.appendChild(productoDiv);
+            
+            if (container) {
+                let ul = container.querySelector('ul');
+                let productoDiv = document.createElement('div');
+                productoDiv.classList.add('card', 'border-info', 'mb-3');
+                productoDiv.style.width = 'fit-content';
+                productoDiv.style.height = 'min-content';
+                productoDiv.innerHTML = `
+                    <img src="${producto.imagen}" alt="${producto.nombre}" style="width: 180px;">
+                <h5 class="card-title">${producto.nombre}</h5>
+                    <p class="card-text">${producto.precio}</p>
+                    <p class="card-text">${producto.descripcion}</p>
+                    <button type="button" class="btn btn-primary" onclick="añadir_carrito(${producto.id})">Añadir al carrito</button>
+                `;
+                ul.appendChild(productoDiv);
+            } else {
+                console.error('Contenedor no encontrado')
+            }
         });
-    });
+    })
+    .catch(error => console.error('Error al obtener productos', error));
 }
+
 function añadir_carrito(id_producto) {
     fetch ('http://localhost:3000/api/v1/productos_seleccionados', {
         method: 'POST',
