7aeff545d0f6f62e508355fee76bd34efba1bd16
sebastian colazo <scolazo@fi.uba.ar>
2025-02-05 12:44:43 -0300

    Cambie de posicion algunas lineas

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const { PrismaClient } = require('@prisma/client')
 const express = require('express')
+
 const usuarios = require('./routes/usuarios')
 const app = express()
 const port = 3000
@@ -12,8 +13,8 @@ app.get('/',(req, res) =>{
     res.send('Kiosco')
 })
 
-app.use('/api/v1/usuarios',usuarios) 
 
+app.use('/api/v1/usuarios',usuarios) 
 
 app.listen(port, () => {
     console.log(`Kiosco app listening on port ${port}`);
