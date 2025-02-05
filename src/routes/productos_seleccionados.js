c346472024d745c4e95096fa461a022e9d0bad62
Thiago <rey.silva.thiago@gmail.com>
2025-02-05 19:45:00 -0300

    Arreglados errores de ortografia

--- src/routes/productos_seleccionados.js
+++ src/routes/productos_seleccionados.js
@@ -6,7 +6,7 @@ const prisma = new PrismaClient()
 //para obtener todos los productos selecionados
 router.get('/', async (req, res) => {
     try{
-        const productos_seleccionados = await prisma.productoSeleccionado.findmany()
+        const productos_seleccionados = await prisma.productos_seleccionados.findMany()
         res.json(productos_seleccionados)
     }catch(error){
         res.status(500).json({mensaje:'Error al obtener los productos seleccionados',error})
@@ -24,15 +24,14 @@ router.put('/:id_comprador/:id_producto', async (req,res)=> {
     }
 
     try{
-        const productoSeleccionado = await prisma.productoSeleccionado.findUnique({
+        const productoSeleccionado = await prisma.productos_seleccionados.findUnique({
             where:{id_comprador_id_producto:{id_comprador: Number(id_comprador), id_producto: Number(id_producto)}}
         })
 
         if(!productoSeleccionado){
             return res.status(404).json({mensaje:'No se encontro el producto'})
         }
-
-        const producto_actualizado = await prisma.productoSeleccionado.update({
+        const producto_actualizado = await prisma.productos_seleccionados.update({
             where: {id_comprador_id_producto:{id_comprador: Number(id_comprador), id_producto: Number(id_producto)}},
             data:{cantidad}
         })
