0b749f3b2350dfe885c35d6813ffe73c94891c22
Thiago <rey.silva.thiago@gmail.com>
2025-02-12 13:41:30 -0300

    Modificaciones para el buen funcionamiento del crear usuario

--- frontend/scripts/crear_cuenta.js
+++ frontend/scripts/crear_cuenta.js
@@ -1,38 +1,29 @@
 function crear(){
-    document.getElementById("guardarBtn").addEventListener("click", async function (event) {
-        event.preventDefault(); 
+    const nombre = document.getElementById("nombre").value;
+    const edad = document.getElementById("edad").value;
+    const mail = document.getElementById("mail").value;
+    const nro_tel = document.getElementById("nro_tel").value;
+    const dni = document.getElementById("dni").value;
+    const contrasenia = document.getElementById("contraseña").value;
 
-        const nombre = document.getElementById("nombre").value;
-        const edad = document.getElementById("edad").value;
-        const mail = document.getElementById("mail").value;
-        const nro_tel = document.getElementById("nro_tel").value;
-        const dni = document.getElementById("dni").value;
-        const contrasenia = document.getElementById("contraseña").value;
+    const datosUsuario = {nombre, edad, mail, nro_tel, dni, contrasenia};
 
-        const datosUsuario = {nombre, edad, mail, nro_tel, dni, contrasenia};
-
-        try {
-        const respuesta = await fetch('http://localhost:3000/api/v1/usuarios', {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(datosUsuario)
-        });
-
-        const data = await respuesta.json();
-
-        if (respuesta.ok) {
+    const respuesta = fetch('http://localhost:3000/api/v1/usuarios', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datosUsuario)
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.mensaje !== 'Usuario creado exitosamente, por favor verifique sus datos') {
+            alert(data.mensaje);
+        } else {
             alert(data.mensaje);
             sessionStorage.setItem("id_usuario", data.id);
             window.location.href = "inicio.html";
-        } else {
-            alert(data.mensaje);
         }
-
-        } catch (error) {
-        console.error("Error al crear usuario:", error);
-        alert('Hubo un problema al crear la cuenta.');
-        }
-    });
+    })
+    .catch(error => console.error("Error al crear usuario:", error));
 }
