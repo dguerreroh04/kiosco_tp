143866a1e02c2d702961bedbc7517919fbedec8f
Thiago <rey.silva.thiago@gmail.com>
2025-02-11 21:01:09 -0300

    Agregada funcionalidad para mostrar la informacion del usuario

--- frontend/scripts/cuenta.js
+++ frontend/scripts/cuenta.js
@@ -1,36 +1,36 @@
+window.onload = function() {
+    mostrar_usuario();
+}
 
-document.addEventListener("DOMContentLoaded", async () => {
-    const nombreInput = document.getElementById("nombre");
-    const edadInput = document.getElementById("edad");
-    const mailInput = document.getElementById("mail");
-    const numTelInput = document.getElementById("nro_tel");
-    const dniInput = document.getElementById("dni");
-    const contraseniaInput = document.getElementById("contraseña");
-    const guardarBtn = document.getElementById("guardarBtn");
+function obtener_id_usuario() {
+    return sessionStorage.getItem("id_usuario");
+}
 
-    try {
-        //mostrar datos ya cargaados
-        const response = await fetch(`http://localhost:3000/api/v1/usuarios/${idUsuario}`);
-        if (!response.ok) {
-            throw new Error(`Error al obtener datos: ${response.statusText}`);
-        }
+function mostrar_usuario() {
+    const id_usuario = obtener_id_usuario()
+    fetch(`http://localhost:3000/api/v1/usuarios/${id_usuario}`)
+    .then(response => response.json())
+    .then(usuario => {
+        console.log(usuario)
+        let container = document.getElementById('datos_usuario');
+        let usuarioDiv = document.createElement('div');
+        usuarioDiv.classList.add('card', 'border-info', 'mb-3');
+        usuarioDiv.style.width = '500px';
+        usuarioDiv.style.height = '160px';
+        usuarioDiv.innerHTML = `
+            <p class="card-title">NOMBRE: ${usuario.nombre}</p>
+            <p class="card-title">EDAD: ${usuario.edad}</p>
+            <p class="card-title">MAIL: ${usuario.mail}</p>
+            <p class="card-title">TELEFONO: ${usuario.nro_tel}</p>
+            <p class="card-title">DNI: ${usuario.dni}</p>
+        `;
+        container.appendChild(usuarioDiv);
+    })
+}
 
-        const usuario = await response.json();
-        
-        nombreInput.value = usuario.nombre || "";
-        edadInput.value = usuario.edad || "";
-        mailInput.value = usuario.mail || "";
-        numTelInput.value = usuario.nro_tel || "";
-        dniInput.value = usuario.dni || "";
-
-    } catch (error) {
-        console.error("Error al cargar los datos: ", error);
-        alert("Hubo un problema al cargar los datos. Intenta nuevamente.");
-    }
-
-function guardar(){
+function guardar() {
     //actulizaacion de datos
-    guardarBtn.addEventListener("click", async (event) => {
+    document.getElementById("guardarBtn").addEventListener("click", async function (event) {
         event.preventDefault();
 
         const datosActualizados = {
@@ -64,4 +64,4 @@ function guardar(){
             alert('Hubo un problema al actualizar los datos de tu cuenta.');
         }
     });
-}});
+};
